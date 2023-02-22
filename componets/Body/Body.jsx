9ca76e0761388767f63088057ca6e7e3c20a8efb
Ravi <ravisankar@86885@gmail.com>
2023-02-22 07:45:55 +0530

    search bar styling

--- componets/Body/Body.jsx
+++ componets/Body/Body.jsx
@@ -15,20 +15,24 @@ function Body() {
   const [restaurantData, setRestaurantData] = useState(Data);
   return (
     <div className="bodycontainer">
-      <input
-        name="search"
-        placeholder="serch resurarent"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          const fdata = onfilter(search);
-          setRestaurantData(fdata);
-        }}
-      >
-        search
-      </button>
+      <div className="searchcontainer">
+        <input
+          name="search"
+          placeholder="serch resurarent"
+          value={search}
+          className="input"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="searchbtn"
+          onClick={() => {
+            const fdata = onfilter(search);
+            setRestaurantData(fdata);
+          }}
+        >
+          search
+        </button>
+      </div>
       <div className="cardContainer">
         {restaurantData?.map((item, index) => (
           <Card {...item.data} />
